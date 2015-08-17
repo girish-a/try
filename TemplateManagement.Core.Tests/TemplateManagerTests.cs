91bfd17a08bcefe6d38f423a3831e4c8dbb27c49
agirish <girish.a@sigmainfo.net>
2015-08-17 18:41:46 +0530

    Razor template management test project

---
+++ TemplateManagement.Core.Tests/TemplateManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TemplateManagement.Core.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class TemplateManagerTests
+    {
+
+        [Fact]
+        public void TestTemplateManager()
+        {
+            RazorTemplateManager rtm = new RazorTemplateManager();
+            string result = rtm.Parse();
+        }
+    }
+}
---
+++ TemplateManagement.Core/RazorTemplateManager.cs
@@ -0,0 +1,28 @@
+﻿using RazorEngine.Templating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemplateManagement.Core
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class RazorTemplateManager
+    {
+        public RazorTemplateManager()
+        {
+        }
+
+        public string Parse()
+        {
+            TemplateService templateService = new TemplateService();
+            string templateName = "TestTemplate";
+            string body = "@model dynamic" + Environment.NewLine + "<html>Hello, @Model.CustomerName!</html>";
+            dynamic model = new { CustomerName = "Girish" };
+
+            var result = templateService.Parse(body, model, null, templateName);
+            return result;
+        }
+    }
+}
